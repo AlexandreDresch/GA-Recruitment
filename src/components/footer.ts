270ee564b0e26988b444e5d12c35e16c3945d73a
AlexandreDresch <connectWithAlexandre@proton.me>
2024-10-18 23:42:35 -0300

    feat: update Footer component

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -2,10 +2,89 @@ import { Component } from "./component";
 
 export class Footer extends Component {
   render(): string {
-    return `
-      <footer>
-        <p>&copy; ${new Date().getFullYear()} Our Company. All rights reserved.</p>
-      </footer>
+    return /*html*/`
+      <footer footer aria-label="Footer">
+      <nav aria-label="Footer Navigation">
+        <p>USE THE NUMBERS TO SWITCH SECTIONS</p>
+        <ul>
+          <li>
+            <a
+              href="/"
+              class="glitch-button small-glitch-button"
+              aria-label="Go to Home page"
+              data-text="00"
+              accesskey="0"
+              >00</a
+            >
+          </li>
+          <li>
+            <a
+              href="#system" 
+              data-text="01"
+              class="glitch-button small-glitch-button"
+              aria-label="Go to The System section"
+              accesskey="1"
+              >01</a
+            >
+          </li>
+          <li>
+            <a
+              href="#archive" 
+              data-text="02"
+              class="glitch-button small-glitch-button"
+              aria-label="Go to Archive section"
+              accesskey="2"
+              >02</a
+            >
+          </li>
+          <li>
+            <a
+              href="#operatives" 
+              data-text="03"
+              class="glitch-button small-glitch-button"
+              aria-label="Go to Operatives section"
+              accesskey="3"
+              >03</a
+            >
+          </li>
+          <li>
+            <a
+              href="#join-us" 
+              data-text="04"
+              class="glitch-button small-glitch-button"
+              aria-label="Go to Join section"
+              accesskey="4"
+              >04</a
+            >
+          </li>
+          <li>
+            <a
+              href="#blame" 
+              data-text="05"
+              class="glitch-button small-glitch-button"
+              aria-label="Go to Blame section"
+              accesskey="5"
+              >05</a
+            >
+          </li>
+        </ul>
+      </nav>
+
+      <div class="">
+        <p>USE ARROWS TO SCROLL</p>
+        <img src="./assets/icons/chevron-down.svg" class="icon" alt="Scroll Icon" />
+      </div>
+
+      <div>
+        <p>&copy; 2024 T.C.A</p>
+        <p>All rights reserved.</p>
+      </div>
+
+      <div class="hidden" aria-label="Contact Information" role="contentinfo">
+        <p>EMAIL:</p>
+        <p>CONTACT@IMG404.ORG</p>
+      </div>
+    </footer>
     `;
   }
 }

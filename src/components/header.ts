6fb69e1ef072400c8a85288debf4e6b72221d002
AlexandreDresch <connectWithAlexandre@proton.me>
2024-10-19 22:44:42 -0300

    feat: ensure hamburger menu closes when a link is clicked

--- src/components/header.ts
+++ src/components/header.ts
@@ -14,7 +14,7 @@ export class Header extends Component {
             <li><a class="glitch-button" href="#system" data-text="THE SYSTEM">THE SYSTEM</a></li>
             <li><a class="glitch-button" href="#archive" data-text="ARCHIVE">ARCHIVE</a></li>
             <li><a class="glitch-button" href="#operatives" data-text="OPERATIVES">OPERATIVES</a></li>
-            <li><a class="cta glitch-button" data-text="JOIN" href="#join">JOIN</a></li>
+            <li><a class="cta glitch-button" data-text="JOIN" href="#join-us">JOIN</a></li>
           </ul>
 
           <div class="hamburger glitch-button" data-text="01101101 01100101 01101110 01110101" id="hamburger">
@@ -23,12 +23,11 @@ export class Header extends Component {
             <span></span>
           </div>
 
-
           <ul id="menu" class="hidden">
             <li><a href="#system">ABOUT</a></li>
             <li><a href="#archive">ARCHIVE</a></li>
             <li><a href="#operatives">OPERATIVES</a></li>
-            <li><a class="cta" href="#join">JOIN</a></li>
+            <li><a class="cta" href="#join-us">JOIN</a></li>
           </ul>
         </nav>
       </header>
@@ -39,8 +38,15 @@ export class Header extends Component {
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger")!;
   const menu = document.getElementById("menu")!;
+  const menuItems = menu.querySelectorAll("li a");
 
   hamburger.addEventListener("click", () => {
     menu.classList.toggle("show");
+  });
+
+  menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+      menu.classList.remove("show");
+    });
   });
 });
